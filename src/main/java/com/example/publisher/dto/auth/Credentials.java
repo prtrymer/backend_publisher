b3f1db6b063ebec53f6535319722df19267becad
prtrymer <chaika.anton@lll.kpi.ua>
2023-12-13 21:57:14 +0200

    made padeable books

--- src/main/java/com/example/publisher/dto/auth/Credentials.java
+++ src/main/java/com/example/publisher/dto/auth/Credentials.java
@@ -15,6 +15,4 @@ public class Credentials {
     @NotBlank(message = "Specify password")
     @Size(min = 6, max = 32, message = "Enter at least 6 and less than 32 characters")
     private String password;
-
-    private boolean admin;
 }
