d6b657e54314dd7839bb813865e7af7118e52e3b
prtrymer <chaika.anton@lll.kpi.ua>
2023-12-14 17:35:39 +0200

    made to recieve to update authors list of idis of books

--- src/main/java/com/example/publisher/security/JwtTokenProvider.java
+++ src/main/java/com/example/publisher/security/JwtTokenProvider.java
@@ -5,7 +5,6 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.publisher.models.Role;
-import com.example.publisher.models.UserEntity;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -53,26 +52,4 @@ public class JwtTokenProvider {
                 .build()
                 .verify(token)
                 .getSubject();
-    }
-    public UserEntity getUserInfo(String jwtToken) {
-        try {
-            Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken).getBody();
-
-            // Extract user information from the decoded token
-            UserInfo userInfo = new UserInfo();
-            userInfo.setUserId(claims.get("user_id", Long.class));
-            userInfo.setUsername(claims.get("username", String.class));
-            userInfo.setEmail(claims.get("email", String.class));
-            // Add more fields as needed
-
-            return userInfo;
-        } catch (ExpiredJwtException e) {
-            // Handle token expiration
-            System.out.println("Token has expired.");
-            return null;
-        } catch (Exception e) {
-            // Handle invalid token or other exceptions
-            System.out.println("Invalid token or other exception.");
-            return null;
-        }
-}
+    }}
