fbbde3df655e8774f3d6e02fd5d1639615e69d26
prtrymer <chaika.anton@lll.kpi.ua>
2023-12-14 17:35:07 +0200

    made to recieve to update authors list of idis of books

--- src/main/java/com/example/publisher/dto/author/AuthorUpdateDto.java
+++ src/main/java/com/example/publisher/dto/author/AuthorUpdateDto.java
@@ -1,15 +1,11 @@
 package com.example.publisher.dto.author;
 
-import com.example.publisher.models.Book;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class AuthorUpdateDto {
-    private int age;
     private String description;
-    @NotNull
-    private List<Book> books;
+    private List<Long> bookIndecies;
 }
--- src/main/java/com/example/publisher/security/JwtTokenProvider.java
+++ src/main/java/com/example/publisher/security/JwtTokenProvider.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.publisher.models.Role;
+import com.example.publisher.models.UserEntity;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -14,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
-public class    JwtTokenProvider {
+public class JwtTokenProvider {
 
     @Value("${jwt.secret}")
     private String jwtSecret;
@@ -53,4 +54,25 @@ public class    JwtTokenProvider {
                 .verify(token)
                 .getSubject();
     }
+    public UserEntity getUserInfo(String jwtToken) {
+        try {
+            Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken).getBody();
+
+            // Extract user information from the decoded token
+            UserInfo userInfo = new UserInfo();
+            userInfo.setUserId(claims.get("user_id", Long.class));
+            userInfo.setUsername(claims.get("username", String.class));
+            userInfo.setEmail(claims.get("email", String.class));
+            // Add more fields as needed
+
+            return userInfo;
+        } catch (ExpiredJwtException e) {
+            // Handle token expiration
+            System.out.println("Token has expired.");
+            return null;
+        } catch (Exception e) {
+            // Handle invalid token or other exceptions
+            System.out.println("Invalid token or other exception.");
+            return null;
+        }
 }
